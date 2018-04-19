5ad7e90798077733d5f1451e515a021ad03a4fdb
Mirko <mirkmark2@gmail.com>
2018-04-19 16:16:00 +0200

    Added material design bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
@@ -10,7 +10,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-
 import { HeroService } from './hero.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ChartsModule } from 'ng2-charts';
 
 
 
@@ -25,8 +27,11 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    ChartsModule
   ],
+  schemas: [NO_ERRORS_SCHEMA ],
   providers: [
     AppService,
     HeroService
